78819fcaa291a25ebf11d6d848b6d1d99595fc1d
shivamkapasia0 <kapasiashivam1@outlook.com>
2021-06-17 12:01:32 +0530

    add headers

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
 
     // Request headers you wish to allow
-
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Accept');
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', false);
